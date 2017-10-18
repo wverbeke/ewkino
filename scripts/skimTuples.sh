899cdcaf6dcf62de209e0aaf5c1c0121b78445ec
wverbeke <willem.verbeke@cern.ch>
2017-10-18 10:26:11 +0200

    Clean up skimming script

--- scripts/skimTuples.sh
+++ scripts/skimTuples.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-CMSSW="CMSSW_9_2_4"
-
-setCMSSW(){
-    echo "cd /user/wverbeke/${CMSSW}/src" >> $1
-    echo "source /cvmfs/cms.cern.ch/cmsset_default.sh" >> $1
-    echo "eval \`scram runtime -sh\`" >> $1
-}
-
-makeSubmit(){
-    > $1
-    setCMSSW $1
-    echo "cd $2" >> $1
-}
+#include bash functiosn to set up CMSSW
+source setCMSSW.sh
 
 skimSample(){                                           #function to skim one sample
     name="${1%/*}"                                      #remove everything before the last "/" in the path to the sample
