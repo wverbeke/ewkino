f397fdcf7396f8202356d1236a613f7522dd96f9
wverbeke <willem.verbeke@cern.ch>
2017-10-20 13:19:05 +0200

    Edit merge tuple script to echo every line

--- scripts/mergeTuples.sh
+++ scripts/mergeTuples.sh
@@ -4,19 +4,17 @@
 source setCMSSW.sh
 
 fillJob(){
-    echo "
-    name=${1%/} \n
-    name=${name##*/} \n
-    name=${name#ntuples_temp_*} \n
-    name=${name}.root \n
-    if [ ! -d ~/Work/ntuples_ewkino ] \n
-        then mkdir ~/Work/ntuples_ewkino \n
-    fi \n
-    if [ -f ~/Work/ntuples_ewkino/$name ] \n
-        then rm ~/Work/ntuples_ewkino/$name \n
-    fi \n
-    hadd ~/Work/ntuples_ewkino/$name ${1}/*root \n
-    " >> $2
+    echo "name=${1%/}" >> $2
+    echo "name=${name##*/}" >> $2
+    echo "name=${name#ntuples_temp_*}" >> $2
+    echo "name=${name}.root" >> $2
+    echo "if [ ! -d ~/Work/ntuples_ewkino ]" >> $2
+    echo "    then mkdir ~/Work/ntuples_ewkino" >> $2
+    echo "fi \n" >> $2
+    echo "if [ -f ~/Work/ntuples_ewkino/$name ]" >> $2
+    echo "    then rm ~/Work/ntuples_ewkino/$name" >> $2
+    echo "fi" >> $2
+    echo "hadd ~/Work/ntuples_ewkino/$name ${1}/*root" >> $2
 }
 
 mergeTuple(){
