fcd77ed7ac72c67d624b185684d7c657d02d54ff
wverbeke <willem.verbeke@cern.ch>
2017-11-08 12:00:29 +0100

    fix categorization

--- src/tZqTools.cc
+++ src/tZqTools.cc
@@ -5,8 +5,8 @@ unsigned tzq::cat(unsigned nJets, unsigned nBJets){
         if(nJets < 2) return 0;
         else return 1;
     } else if(nBJets == 1){
-        if(nJets < 1) return 2;
-        else if(nJets < 2) return 3;
+        if(nJets < 2) return 2;
+        else if(nJets < 4) return 3;
         else return 4;
     } else{
         return 5;
