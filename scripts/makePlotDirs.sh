8b06a094e84d69b220af5edfce4e02dc13d755e8
wverbeke <willem.verbeke@cern.ch>
2017-11-08 16:12:59 +0100

    expand setup script to work for all categories

--- scripts/makePlotDirs.sh
+++ scripts/makePlotDirs.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 mkdir -p ../plots/
 cd ../plots/
+mkdir -p ttgamma
+mkdir -p ewkino
+mkdir -p tZq
+#set up ewkino plots
+cd ewkino
+mkdir -p dilepCR
+cd dilepCR
 #make directories for every run
 for dir in all2017 RunA RunB RunC RunD RunE RunF
     do mkdir -p $dir
@@ -10,3 +17,18 @@ for dir in ./*
         do mkdir -p $dir/$subdir
     done
 done
+cd ../..
+
+#set up ttgamma plots
+cd ttgamma
+for dir in inclusive ee em mm
+    do mkdir -p $dir
+done
+cd ..
+
+#set up tZq plots 
+cd tZq 
+for dir in inclusive 0bJets_01Jets 0bJets_2Jets 1bJet_01jets 1bJet_23Jets 1bJet_3Jets 2bJets 
+    do mkdir -p $dir
+done 
+cd ..
