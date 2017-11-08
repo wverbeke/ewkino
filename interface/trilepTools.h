b61d63db9f3045fb439bcf85617c971e41acdc37
wverbeke <willem.verbeke@cern.ch>
2017-11-08 12:49:18 +0100

    Add comments

--- interface/trilepTools.h
+++ interface/trilepTools.h
@@ -7,6 +7,6 @@
 namespace trilep{
     std::pair<unsigned, unsigned> bestZ(const TLorentzVector*, const unsigned); //return indices of leptons forming best Z-candidate
     unsigned flavorChargeComb(const std::vector<unsigned>& ind, const unsigned*, const int*, const unsigned); //Determine whether event contains OSSF or OSOF pair, or not 
-    std::pair<double, double> neutrinoPZ(const TLorentzVector& wLep, const TLorentzVector& met);
+    std::pair<double, double> neutrinoPZ(const TLorentzVector& wLep, const TLorentzVector& met);              //solve the neutrino Z momentum from the W mass constraint
 }
 #endif
