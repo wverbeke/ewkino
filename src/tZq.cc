3eed5c8adf0c2cacb7cff89d33abc132bbe9ed07
wverbeke <willem.verbeke@cern.ch>
2017-11-08 17:17:58 +0100

    plot tZq on top

--- src/tZq.cc
+++ src/tZq.cc
@@ -168,12 +168,12 @@ void treeReader::Analyze(){
             }
         }
     }
-    const bool isSMSignal = {true, false, false, false, false, false, false};
+    const bool isSMSignal[ (const size_t) proc.size() ] = {true, false, false, false, false, false, false};
     //plot all distributions
     for(unsigned cat = 0; cat < nCat; ++cat){
         for(unsigned dist = 0; dist < nDist; ++dist){
-            plotDataVSMC(mergedHists[cat][dist][0], &mergedHists[cat][dist][1], &proc[0], mergedHists[cat][dist].size() - 1, "tZq/" + std::get<0>(histInfo[dist]), "ewkino", false, true);             //linear plots
-            plotDataVSMC(mergedHists[cat][dist][0], &mergedHists[cat][dist][1], &proc[0], mergedHists[cat][dist].size() - 1, "tZq/" + std::get<0>(histInfo[dist])  + "_log", "ewkino", true, true);    //log plots
+            plotDataVSMC(mergedHists[cat][dist][0], &mergedHists[cat][dist][1], &proc[0], mergedHists[cat][dist].size() - 1, "tZq/" + std::get<0>(histInfo[dist]), "ewkino", false, true, "", nullptr, isSMSignal);             //linear plots
+            plotDataVSMC(mergedHists[cat][dist][0], &mergedHists[cat][dist][1], &proc[0], mergedHists[cat][dist].size() - 1, "tZq/" + std::get<0>(histInfo[dist])  + "_log", "ewkino", true, true, "", nullptr, isSMSignal);    //log plots
         }
     }
 }
