f3c9d784c368d615dcf0245a3e949c5d1cb7f52d
wverbeke <willem.verbeke@cern.ch>
2017-11-08 15:24:34 +0100

    Add function to reconstrunct top mass and tag jets

--- interface/tZqTools.h
+++ interface/tZqTools.h
@@ -1,7 +1,13 @@
 #ifndef tZqTools_H
 #define tZqTools_H
 
+//include c++ library headers
+
+//include ROOT headers
+#include "TLorentzVector.h"
+
 namespace tzq{
     unsigned cat(unsigned nJets, unsigned nBJets);
+    double findMTop(const TLorentzVector& wLep, const TLorentzVector& met, std::vector<unsigned>& taggedJetI, const std::vector<unsigned>& jetI, const TLorentzVector* jetV, const std::vector<unsigned>& bJetI);
 }
 #endif
--- src/tZqTools.cc
+++ src/tZqTools.cc
@@ -1,4 +1,5 @@
 #include "../interface/tZqTools.h"
+#include "../interface/trilepTools.h"
 
 unsigned tzq::cat(unsigned nJets, unsigned nBJets){
     if(nBJets == 0){
@@ -11,4 +12,46 @@ unsigned tzq::cat(unsigned nJets, unsigned nBJets){
     } else{
         return 5;
     }
+}
+
+double tzq::findMTop(const TLorentzVector& wLep, const TLorentzVector& met, std::vector<unsigned>& taggedJetI, const std::vector<unsigned>& jetI, const TLorentzVector* jetV, const std::vector<unsigned>& bJetI){
+    static const double mTop = 173.1;
+    std::pair<double, double> pzSol = trilep::neutrinoPZ(wLep, met);
+    TLorentzVector neutrinoPlus(met.Px(), met.Py(), pzSol.first, met.Px()*met.Px() + met.Py()*met.Py() + pzSol.first*pzSol.first);
+    TLorentzVector neutrinoMin(met.Px(), met.Py(), pzSol.second, met.Px()*met.Px() + met.Py()*met.Py() + pzSol.second*pzSol.second);
+    bool plus;  //true if + solution is chosen, otherwise false
+    if(bJetI.size() == 0){
+        taggedJetI[0] = jetI[0];
+    } else if(bJetI.size() == 1){
+        taggedJetI[1] = bJetI[0];
+    } else{
+        double minDiff = 999999.;
+        for(unsigned b = 0; b < bJetI.size(); ++b){
+            double diffPlus = fabs( (neutrinoPlus + wLep + jetV[bJetI[b]] ).M() - mTop);
+            double diffMin = fabs( (neutrinoMin + wLep + jetV[bJetI[b]] ).M() - mTop);
+            if(diffPlus < minDiff && diffPlus < diffMin){
+                plus = true;
+                taggedJetI[0] = bJetI[b];
+            } else if(diffMin < minDiff){
+                plus = false;
+                taggedJetI[0] = bJetI[b];
+            }
+        }
+    }
+    if(bJetI.size() < 2){
+        if(fabs( (neutrinoPlus + wLep + jetV[taggedJetI[0]] ).M() - mTop)  < fabs( (neutrinoMin + wLep + jetV[taggedJetI[0]] ).M() - mTop) ){
+            plus = true;
+        } else{
+            plus = false;
+        }
+    }
+    //find recoiling jet ( := leading jet not considered the b-jet from top)
+    for(unsigned j = 0; j < jetI.size(); ++j){
+        if(jetI[j] != taggedJetI[0]){
+            taggedJetI[1] = jetI[j];
+            break;                              //jet collection is assumed oredered
+        }
+    }
+    if(plus) return ( neutrinoPlus + wLep + jetV[taggedJetI[0]] ).M();
+    else     return ( neutrinoMin  + wLep + jetV[taggedJetI[0]] ).M();
 }
