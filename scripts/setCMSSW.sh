caf0ac0f7d8813dd8c16a4ecdc15b4f2683c9c1e
wverbeke <willem.verbeke@cern.ch>
2017-11-10 12:16:06 +0100

    generalize setCMSSW script

--- scripts/setCMSSW.sh
+++ scripts/setCMSSW.sh
@@ -2,8 +2,15 @@
 
 CMSSW="CMSSW_9_2_4"
 
+if [ ! -d /user/${USER}/${CMSSW} ]; then
+    cd ~
+    cmsrel CMSSW_9_2_4
+    cd CMSSW_9_2_4/src
+    cmsenv
+fi
+
 setCMSSW(){
-    echo "cd /user/wverbeke/${CMSSW}/src" >> $1
+    echo "cd /user/${USER}/${CMSSW}/src" >> $1
     echo "source /cvmfs/cms.cern.ch/cmsset_default.sh" >> $1
     echo "eval \`scram runtime -sh\`" >> $1
 }
