301c89872034833fcca2bf9d232d600a21113ab8
wverbeke <willem.verbeke@cern.ch>
2017-11-13 17:37:27 +0100

    change multiboson color

--- plotting/plotCode.cc
+++ plotting/plotCode.cc
@@ -95,7 +95,7 @@ Color_t bkgColortZq(const std::string& bkgName){
     if(bkgName == "TT" ||  bkgName == "TT + Jets") return kOrange - 2;
     if(bkgName == "TT/T + X" || bkgName == "TT + X") return kBlue - 10;
     if(bkgName == "X + #gamma") return kMagenta - 10;
-    if(bkgName == "multiboson") return kRed - 8;
+    if(bkgName == "multiboson") return kYellow - 9;
     if(bkgName == "Drell-Yan" || bkgName == "DY") return kViolet - 7;
     if(bkgName == "WJets") return kSpring + 2;
     if(bkgName == "TT + Z") return kBlue - 6; 
