76ac72698a9d70cbef724253a1c77dbcbe9e2fe8
willem <willem.verbeke@cern.ch>
2017-11-16 11:22:44 +0100

    fix bug

--- bdtTraining/trainingWrapper.sh
+++ bdtTraining/trainingWrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for 0bJets_01Jets 0bJets_2Jets 1bJet_01jets 1bJet_23Jets 1bJet_4Jets 2bJets; do
+for jetCat in 0bJets_01Jets 0bJets_2Jets 1bJet_01jets 1bJet_23Jets 1bJet_4Jets 2bJets; do
     for mllCat in onZ offZ; do
         ./trainBDT $jetCat $mllCat
         for method in BDT BDTG BDTD BDTB; do
