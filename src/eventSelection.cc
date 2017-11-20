3dd2a36ebc5c9d6a13df18e24b81154815e351b1
willem <willem.verbeke@cern.ch>
2017-11-20 19:16:19 +0100

    implement overlap removal functions

--- src/eventSelection.cc
+++ src/eventSelection.cc
@@ -162,3 +162,23 @@ unsigned treeReader::nBJets(std::vector<unsigned>& bJetInd, const unsigned unc, 
     orderByPt(bJetInd, _jetPt, nbJets);
     return nbJets;
 }
+
+//overlap removal between events
+
+bool treeReader::photonOverlap(){
+    if(std::get<1>(samples[currentSample - 1]).find("DYJetsToLL") != std::string::npos){
+        return _zgEventType > 2; 
+    } else if(std::get<1>(samples[currentSample - 1]).find("TTTo2L") != std::string::npos){
+        return _ttgEventType > 2;
+    }
+    return false;
+}
+
+bool treeReader::htOverlap(){
+    if(std::get<1>(samples[currentSample - 1]).find("DYJetsToLL_M-50_Tune") != std::string::npos){
+        return _gen_HT > 70.;
+    } else if(std::get<1>(samples[currentSample - 1]).find("DYJetsToLL_M-10_50_Tune") != std::string::npos){
+        return _gen_HT > 100.;
+    }
+    return false;
+}
