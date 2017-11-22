320ab95874547fa7cd64d859fcd2882546c6093b
willem <willem.verbeke@cern.ch>
2017-11-22 17:04:43 +0100

    restore submission

--- bdtTraining/trainingWrapperSubmit.sh
+++ bdtTraining/trainingWrapperSubmit.sh
@@ -9,8 +9,7 @@ for jetCat in  0bJets_01Jets 0bJets_2Jets 1bJet_01jets 1bJet_23Jets 1bJet_4Jets 
         echo "for method in BDT BDTG BDTD BDTB MLP DNN; do" >> mvaTraining.sh
         echo '  mv dataset/weights/TMVAClassification_${method}.weights.xml submitWeights/${jetCat}_${mllCat}_${method}.weights.xml' >> mvaTraining.sh
         echo "done">> mvaTraining.sh
-        #qsub mvaTraining.sh -q highmem -lnodes=1:ppn=16 -l walltime=20:00:00
-        cat mvaTraining.sh
+        qsub mvaTraining.sh -q highmem -lnodes=1:ppn=16 -l walltime=20:00:00
     done
 done
 rm mvaTraining.sh
