e7d102eed0d92dc8da44dc1ff5815f479a701e05
willem <willem.verbeke@cern.ch>
2017-11-22 11:47:08 +0100

    make cluster version of training wrapper

---
+++ bdtTraining/trainingWrapperSubmit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source ../scripts/setCMSSW.sh
+
+for jetCat in 0bJets_01Jets 0bJets_2Jets 1bJet_01jets 1bJet_23Jets 1bJet_4Jets 2bJets; do
+    for mllCat in onZ offZ; do
+        > mvaTraining.sh
+        makeSubmit mvaTraining.sh /user/wverbeke/Work/AnalysisCode/ewkino/bdtTraining/
+        echo "./trainBDT $jetCat $mllCat" >> mvaTraining.sh
+        echo "for method in BDT BDTG BDTD BDTB MLP DNN; do" >> mvaTraining.sh
+        echo "  mv dataset/weights/TMVAClassification_${method}.weights.xml bdtWeights/${jetCat}_${mllCat}_${method}.weights.xml" >> mvaTraining.sh
+        echo "done">> mvaTraining.sh
+        qsub mvaTraining.sh -lnodes=1:ppn=8 -l walltime=20:00:00
+    done
+done
+rm mvaTraining.sh
