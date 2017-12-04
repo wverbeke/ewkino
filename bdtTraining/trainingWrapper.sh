015f78f8ee8c4c250b6b597878d6fcea73c8779e
willem <willem.verbeke@cern.ch>
2017-12-04 14:00:56 +0100

    add residual changes

--- bdtTraining/trainingWrapper.sh
+++ bdtTraining/trainingWrapper.sh
@@ -2,7 +2,7 @@
 for jetCat in 0bJets_01Jets 0bJets_2Jets 1bJet_01jets 1bJet_23Jets 1bJet_4Jets 2bJets; do
     for mllCat in onZ offZ; do
         ./trainBDT $jetCat $mllCat
-        for method in BDT BDTG BDTD BDTB MLP DNN; do
+        for method in BDT BDTG BDTGAlt BDTGAlt_200trees BDTGAlt_shrinkage04 BDTGAlt_minNode005 BDTGAlt_negWeights BDTGAlt_MaxDepth4 BDTGAlt_20Cuts BDTGOtherAn BDTD BDTB MLP DNN; do
             mv dataset/weights/TMVAClassification_${method}.weights.xml bdtWeights/${jetCat}_${mllCat}_${method}.weights.xml
         done
     done
