1b8eddd2a743379a5f77d6a4916ef7b4c27fa256
willem <willem.verbeke@cern.ch>
2017-12-14 17:13:26 +0100

    fix pu weight code

--- src/extractPuWeights.cc
+++ src/extractPuWeights.cc
@@ -16,16 +16,16 @@ void extractPuWeight(const std::string& mcFile){
     mcPuDist->Scale(1/mcPuDist->GetSumOfWeights());
     //pu weights
     TH1D* puWeights[6]; 
-    for(unsigned e = 0; e < 6; ++e) puWeights[e] = (TH1D*) mcPuDist->Clone(); 
+    TFile* dataFile[6];
     //read data pu distributions 
     const std::string eras[6] = {"Inclusive", "B", "C", "D", "E", "F"};
     for(unsigned e = 0; e < 6; ++e){
-        TFile* dataFile = TFile::Open( (const TString&) "weights/pileUpData/dataPuHist_era" + eras[e] + ".root");
-        TH1D* dataHist = (TH1D*) dataFile->Get("pileup");
+        dataFile[e] = TFile::Open( (const TString&) "weights/pileUpData/dataPuHist_era" + eras[e] + ".root");
+        TH1D* dataHist = (TH1D*) dataFile[e]->Get("pileup");
         //normalize histogram to unity
-        dataHist->Scale(1/dataHist->GetSumOfWeights());
-        puWeights[e]->Divide(dataHist);
-        dataFile->Close();
+        puWeights[e] = (TH1D*) dataHist->Clone();
+        puWeights[e]->Scale(1/dataHist->GetSumOfWeights());
+        puWeights[e]->Divide(mcPuDist);
     }
     //write pu Weights to file
     TFile* outputFile = TFile::Open("weights/puWeights2017.root", "RECREATE");
@@ -34,6 +34,7 @@ void extractPuWeight(const std::string& mcFile){
     }
     outputFile->Close();
     inputFile->Close();
+    for(unsigned e = 0; e < 6; ++e) dataFile[e]->Close();
 }
 
 int main(int argc, char* argv[]){
