08fd2678ce91e2182b077d2a42ea9ed01ef2267b
wverbeke <willem.verbeke@cern.ch>
2018-01-11 15:44:39 +0100

    further develop histCollection

--- interface/HistCollection.h
+++ interface/HistCollection.h
@@ -19,12 +19,16 @@ Required inputs are a collection of HistInfo objects and a categorization.
 #include "Sample.h"
 
 class HistCollectionSample{
+    friend HistCollectionSample operator+(const HistCollectionSample&, const HistCollectionSample&); 
+    friend class HistCollection;
     public:
+        HistCollectionSample() = default;
         HistCollectionSample(const std::vector<HistInfo>&, std::shared_ptr<Category>, std::shared_ptr<Sample>);
         HistCollectionSample(const std::vector<HistInfo>&, const std::vector< std::vector< std::string > >&, std::shared_ptr<Sample>);
         std::shared_ptr<TH1D> access(size_t, const std::vector<size_t>&) const; //access specific histogram
         void store(const std::string&);                                         //write all histograms to ROOT files in given location
         void setNegZero();
+        HistCollectionSample& operator+=(const HistCollectionSample&);
     private:
         std::vector< std::vector < std::shared_ptr<TH1D> > > collection;
         std::shared_ptr<Sample> sample;
@@ -33,6 +37,7 @@ class HistCollectionSample{
 
 class HistCollection{
     public:
+        HistCollection() = default;
         HistCollection(const std::vector<HistInfo>&, std::shared_ptr<Category>, const std::vector<Sample>&);
         HistCollection(const std::vector<HistInfo>&, const std::vector< std::vector < std::string > >&, const std::vector<Sample>&);
         std::shared_ptr<TH1D> access(size_t, size_t, const std::vector<size_t>&) const;
--- src/HistCollection.cc
+++ src/HistCollection.cc
@@ -1,5 +1,8 @@
 #include "../interface/HistCollection.h"
 
+//include c++ library classes
+#include <iostream>
+#include <set>
 
 HistCollectionSample::HistCollectionSample(const std::vector<HistInfo>& infoList, std::shared_ptr< Category > categorization, std::shared_ptr<Sample> sam): cat(categorization), sample(sam){
     for(auto infoIt = infoList.cbegin(); infoIt != infoList.cend(); ++infoIt){
@@ -20,6 +23,35 @@ std::shared_ptr<TH1D> HistCollectionSample::access(size_t infoIndex, const std::
     return collection[infoIndex][catIndex];
 }
 
+void HistCollectionSample::setNegZero(){
+    for(auto dIt = collection.begin(); dIt != collection.cend(); ++dIt){
+        for(auto cIt = dIt->cbegin(); cIt != dIt->cend(); ++cIt){
+            for(unsigned b = 1; b < (*cIt)->GetNbinsX() + 1; ++b){
+                if((*cIt)->GetBinContent(b) < 0.) (*cIt)->SetBinContent(b, 0.);
+            }
+        }
+    }
+}
+
+HistCollectionSample& HistCollectionSample::operator+=(const HistCollectionSample& rhs){
+    if(collection.size() != rhs.collection.size() || cat->getCat().size() != rhs.cat->getCat().size()){
+        std::cerr << "HistCollection of incompatible dimensions can not be added: returning left hand side!" << std::endl;
+    } else{
+        for(size_t dist = 0; dist < collection.size(); ++dist){
+            for(size_t c = 0; c < collection[dist].size(); ++c){
+                collection[dist][c]->Add(rhs.collection[dist][c].get());
+            }
+        }
+    }
+    return *this;
+};
+
+HistCollectionSample operator+(const HistCollectionSample& lhs, const HistCollectionSample& rhs){
+    HistCollectionSample ret(lhs);
+    ret += rhs;
+    return ret;
+}
+
 
 HistCollection::HistCollection(const std::vector<HistInfo>& infoList, std::shared_ptr< Category > categorization, const std::vector<Sample>& samList){ 
     for(auto samIt = samList.cbegin(); samIt != samList.cend(); ++samIt){
@@ -30,3 +62,30 @@ HistCollection::HistCollection(const std::vector<HistInfo>& infoList, std::share
 HistCollection::HistCollection(const std::vector<HistInfo>& infoList, const std::vector < std::vector < std::string > >& categorization, const std::vector<Sample>& samList):
     HistCollection(infoList, std::make_shared<Category>(Category(categorization)), samList) {}
 
+std::shared_ptr<TH1D> HistCollection::access(size_t samIndex, size_t infoIndex, const std::vector<size_t>& catIndices) const{
+    return fullCollection[samIndex].access(infoIndex, catIndices);
+}
+
+void HistCollection::setNegZero(){
+    for(HistCollectionSample& samCol : fullCollection){
+        samCol.setNegZero();
+    }
+}
+
+void HistCollection::mergeProcesses(){
+    setNegZero();
+    HistCollection tempCol;
+    std::set<std::string> usedProcesses;
+    for(auto it = fullCollection.cbegin(); it != fullCollection.cend(); ++it){
+        if(usedProcesses.find(it->sample->getProc()) != usedProcesses.end()){
+            HistCollectionSample tempSam = *it;
+            for(auto jt = it + 1; jt != fullCollection.cend(); ++jt){
+                if(it->sample->getProc() == jt->sample->getProc()){
+                    tempSam += *jt;
+                }
+            }
+            tempCol.fullCollection.push_back(tempSam);
+        }
+    }
+    *this = tempCol;
+}
