2c4e618e7f8707b3fc7d014b6f8a1f4b7da720e3
wverbeke <willem.verbeke@cern.ch>
2018-01-18 11:14:11 +0100

    Add plot class

---
+++ interface/Plot.h
@@ -0,0 +1,29 @@
+/*
+Class containing data and background histograms for plotting
+*/
+
+//include c++ library classes
+#include <map>
+#include <string>
+
+//include root classes
+
+
+#ifndef Plot_H
+#define Plot_H
+class Plot{
+    public:
+        Plot(const std::string& fileName, std::shared_ptr<TH1D> obs, std::map< std::string, std::shared_ptr < TH1D > > back, std::map< std::string, 
+            std::shared_ptr< TH1D > > unc = std::map< std::string, std::shared_ptr< TH1D > >(), std::map< std::string, std::shared_ptr< TH1D > > sig = std::map< std::string, std::shared_ptr< TH1D > >() ):
+            data(obs), bkg(back), syst(unc), signal(sig) {}
+        Plot(const std::string& fileName, std::shared_ptr<TH1D> obs, const HistInfo& info, const HistCollection& collection);
+
+        void draw(const std::string& analysis = "", bool log = false, bool normToData = false, const std::string& header = "", TH1D** bkgSyst = nullptr, const bool* isSMSignal = nullptr, const bool sigNorm = true);
+    private:
+        std::shared_ptr<TH1D> data;
+        std::map< std::string, std::shared_ptr< TH1D > > bkg;
+        std::map< std::string, std::shared_ptr< TH1D > > syst; 
+        std::map< std::string, std::shared_ptr< TH1D > > signal;
+        std::string fileName;
+};
+#endif
