eb02de361b9e17d61cb15ae1dea891d128cd3b19
willem <willem.verbeke@cern.ch>
2018-02-14 16:54:42 +0100

    fix scripts

--- scripts/makePlotDirs.sh
+++ scripts/makePlotDirs.sh
@@ -35,7 +35,7 @@ cd ..
 cd tZq 
 for dir1 in 2016 2017; do
     for dir2 in mllInclusive onZ offZ; do
-        for dir3 in inclusive 0bJets_01Jets 0bJets_2Jets 1bJet_01jets 1bJet_23Jets 1bJet_4Jets 2bJets; do
+        for dir3 in nJetsInclusive 0bJets01Jets 0bJets2Jets 1bJet01jets 1bJet23Jets 1bJet4Jets 2bJets; do
             mkdir -p ${dir1}/${dir2}/${dir3}
         done
     done 
