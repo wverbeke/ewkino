da6c5197d9536f92c3b20ac07fdc667375fbc354
willem <willem.verbeke@cern.ch>
2018-02-14 14:55:58 +0100

    fix script

--- scripts/makePlotDirs.sh
+++ scripts/makePlotDirs.sh
@@ -33,17 +33,25 @@ cd ..
 
 #set up tZq plots 
 cd tZq 
-
-mkdir tZq/dataFakes/
-cd tZq/dataFakes/
-for d in . 36fb 81fb; do
-    for dir in mllInclusive onZ offZ; do
-        for subdir in nJetsInclusive 0bJets_01Jets 0bJets_2Jets 1bJet_01jets 1bJet_23Jets 1bJet_4Jets 2bJets
-            do mkdir -p ${d}/${dir}/${subdir}
+for dir1 in 2016 2017; do
+    for dir2 in mllInclusive onZ offZ; do
+        for dir3 in inclusive 0bJets_01Jets 0bJets_2Jets 1bJet_01jets 1bJet_23Jets 1bJet_4Jets 2bJets; do
+            mkdir -p ${dir1}/${dir2}/${dir3}
         done
-    done
+    done 
 done
-cd ../..
+cd ..
+
+#mkdir tZq/dataFakes/
+#cd tZq/dataFakes/
+#for d in . 36fb 81fb; do
+#    for dir in mllInclusive onZ offZ; do
+#        for subdir in nJetsInclusive 0bJets_01Jets 0bJets_2Jets 1bJet_01jets 1bJet_23Jets 1bJet_4Jets 2bJets
+#            do mkdir -p ${d}/${dir}/${subdir}
+#        done
+#    done
+#done
+#cd ../..
 
 #mkdir tZq/controlR/
 #cd tZq/controlR/
