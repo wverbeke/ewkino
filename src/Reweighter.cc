e064530d93ccaba21704eae513fed02ddcfa088c
wverbeke <willem.verbeke@cern.ch>
2018-02-21 00:05:05 +0100

    start cleaning up Reweighter

--- src/Reweighter.cc
+++ src/Reweighter.cc
@@ -6,13 +6,17 @@
 #include "TFile.h"
 
 Reweighter::Reweighter(){
+
     //initialize pu weights
     const std::string vars[3] = {"central", "down", "up"};
     for(unsigned v = 0; v < 3; ++v){
-        TFile* puFile = TFile::Open((const TString&) "weights/puw_nTrueInt_Moriond2017_36p5fb_Summer16_" + vars[v] + ".root");
+        TFile* puFile = TFile::Open( (const TString&) "weights/puw_nTrueInt_Moriond2017_36p5fb_Summer16_" + vars[v] + ".root");
         puWeights[v] = (TH1D*) puFile->Get("puw");
-        //puFile->Close();
+        //make sure histogram does not get deleted when closing file
+        puWeights[v]->SetDirectory(gROOT);
+        puFile->Close();
     }
+
     //initialize b-tag weights
     //currently assuming medium WP of deepCSV tagger 
     bTagCalib = new BTagCalibration("deepCsv", "weights/DeepCSV_Moriond17_B_H.csv");
@@ -20,31 +24,42 @@ Reweighter::Reweighter(){
     bTagCalibReader->load(*bTagCalib, BTagEntry::FLAV_B, "comb");
     bTagCalibReader->load(*bTagCalib, BTagEntry::FLAV_C, "comb");
     bTagCalibReader->load(*bTagCalib, BTagEntry::FLAV_UDSG, "incl");    
+
     //initialize b-tag efficiencies
     TFile* bTagFile = TFile::Open("weights/bTagEff_deepCSV_medium_cleaned_ewkino.root");
     const std::string quarkFlavors[3] = {"udsg", "charm", "beauty"};
     for(unsigned flav = 0; flav < 3; ++flav){
-        bTagEffHist[flav] = (TH1D*) bTagFile->Get((const TString&) "bTagEff_" + quarkFlavors[flav]);
+        bTagEffHist[flav] = (TH1D*) bTagFile->Get( (const TString&) "bTagEff_" + quarkFlavors[flav]);
+        bTagEffHist[flav]->SetDirectory(gROOT);
     }
-    //bTagFile->Close();
+    bTagFile->Close();
+
     //Read Muon reco SF Weights
     TFile* muonRecoFile = TFile::Open("weights/muonTrackingSF_2016.root");
     muonRecoSF = (TGraph*) muonRecoFile->Get("ratio_eff_eta3_dr030e030_corr");
+    muonRecoSF->SetDirectory(gROOT);
+    muonRecoFile->Close();
+
     //Read Electron reco SF Weights
     TFile* electronRecoFile = TFile::Open("weights/electronRecoSF_2016.root");
     electronRecoSF = (TH2D*) electronRecoFile->Get("EGamma_SF2D");
+    electronSF->SetDirectory(gROOT);
+    electronRecoFile->Close();
+
     //Read muon id SF weights
     TFile* muonMediumFile = TFile::Open("weights/muonScaleFactors_MediumIDtoReco.root");
+    muonMediumSF = (TH2D*) muonMediumFile->Get("SF");
+    muonMediumSF->Close();
+    muonMediumFile->Close();
+
     TFile* muonMiniIsoFile = TFile::Open("weights/muonScaleFactors_miniIso0p4toMediumID.root");
     TFile* muonIPFile = TFile::Open("weights/muonScaleFactors_dxy0p05dz0p1toMediumID.root");
     TFile* muonSIP3DFile = TFile::Open("weights/muonScaleFactors_sip3d4toMediumID.root");
     TFile* muonLeptonMvaFile = TFile::Open("weights/muonScaleFactors_ttHMvaTight_old.root");
-    muonMediumSF = (TH2D*) muonMediumFile->Get("SF");
     muonMiniIsoSF = (TH2D*) muonMiniIsoFile->Get("SF");
     muonIPSF = (TH2D*) muonIPFile->Get("SF");
     muonSIP3DSF = (TH2D*) muonSIP3DFile->Get("SF");
     muonLeptonMvaSF = (TH2D*) muonLeptonMvaFile->Get("sf");
-    //muonMediumFile->Close();
     //muonMiniIsoFile->Close();
     //muonIPFile->Close();
     //muonSIP3DFile->Close();
