c9e43899bbdeba9efbe5bec3ba13aaff3358b77d
wverbeke <willem.verbeke@cern.ch>
2018-03-12 17:10:41 +0100

    factorize BDTReader implementation

---
+++ interface/CategorizedBDTReader.h
@@ -0,0 +1,39 @@
+/*
+multi category bdt reader
+*/
+
+#ifndef CategorizedBDTReader_H
+#define CategorizedBDTReader_H
+
+//include other parts of code 
+#include "../interface/BDTReader.h"
+
+//include c++ library functions
+#include <memory>
+#include <vector>
+#include <map>
+#include <string>
+
+//include ROOT classes
+
+class CategorizedBDTReader{
+     public:
+        CategorizedBDTReader(const std::string&, const std::string&, const std::shared_ptr<Category>&, const std::map< std::string, float>&);
+        CategorizedBDTReader(const std::string&, const std::string&, const Category&, const std::map < std::string, float >&);
+        CategorizedBDTReader(const std::string&, const std::string&, const std::vector < std::vector < std::string > >&, const std::map < std::string, float >&);
+
+        //get the BDT output for this category's BDT 
+        float computeBDT(const std::vector< size_t > &, const std::map < std::string, float>& );
+
+    private:
+        std::shared_ptr<Category> category;
+        std::vector<BDTReader> bdtReaders;
+        std::string methodName;
+
+        //get name of method (xml file) for given category
+        std::string mvaMethodName(const size_t) const;
+
+        //get the BDT output for this category's BDT 
+        float computeBDT(const size_t categoryIndex, const std::map < std::string, float>& );
+};
+#endif
--- src/BDTReader.cc
+++ src/BDTReader.cc
@@ -1,52 +1,26 @@
 #include "../interface/BDTReader.h"
 
-BDTReader::BDTReader(const std::string& bdtName, const std::shared_ptr< Category >& cat, const std::map < std::string, float>& varMap):
-    methodName(bdtName), category(cat), variableMap(varMap){
-    for(size_t c = 0; c < category->size(); ++c){
-        readers.push_back( std::shared_ptr<TMVA::Reader> ( new TMVA::Reader( "!Color:!Silent" ) ) );
-        //set all variables         
-        addVariables(readers[c]);
-        //book method
-        readers[c]->BookMVA(methodName, mvaMethodName(c) + ".xml");
-    }            
-
+BDTReader::BDTReader(const std::string& bdtName, const std::string& xmlFileName, const std::shared_ptr< std::map < std::string, float > >& varMap):
+    methodName(bdtName), variableMap(varMap)
+{
+    reader.reset( new TMVA::Reader("!Color:!Silent") );
+    //set all variables
+    addVariables();
+    //book method 
+    reader->BookMVA(methodName, xmlFileName); 
 }
 
-void BDTReader::addVariables(const std::shared_ptr< TMVA::Reader >& reader){
-    for(auto mapIt = variableMap.begin(); mapIt != variableMap.end(); ++mapIt){
-        reader->AddVariable( (const TString&) mapIt->first, &variableMap[mapIt->first]);
+
+void BDTReader::addVariables() const{
+    for(auto& variable : *variableMap){
+        reader->AddVariable( (const TString&) variable.first, &( (*variableMap)[variable.first] ) );
     }
 }
 
-std::string BDTReader::mvaMethodName(const size_t categoryIndex) const{
-    return ( category->name(categoryIndex) + "_" + methodName );
-}
-
-//compute mva value for given category's mva
-float BDTReader::computeBDT(const size_t categoryIndex, const std::map < std::string, float>& varMap){
-
-    //check if input map has the correct size
-    /*
-    if(variableMap.size() != varMap.size()){
-        std::cerr << "Error: trying to set TrainingTree map equal to a map of different size! returning control" << std::endl;
-        return 0.;
-    }
-
-    //optimized way of copying the map's floats 
-    auto variableMapIt = variableMap.begin();
-    for(auto tempIt = varMap.cbegin(); tempIt != varMap.cend(); ++tempIt, ++variableMapIt){
-        variableMapIt->second = tempIt->second;
-    }
-    */
+float BDTReader::computeBDT(const std::map < std::string, float>& varMap){
     for(auto it = varMap.cbegin(); it != varMap.cend(); ++it){
-        variableMap[it->first] = it->second;
+        (*variableMap)[it->first] = it->second;
     }
-
     //retrieve bdt output and return
-    return ( readers[categoryIndex]->EvaluateMVA(methodName) );
-}
-
-float BDTReader::computeBDT(const std::vector<size_t>& categoryIndices, const std::map < std::string, float>& varMap){
-    size_t categoryIndex = category->getIndex(categoryIndices);
-    return computeBDT(categoryIndex, varMap);
+    return ( reader->EvaluateMVA(methodName) );
 }
