10ca50ff0a01193f76b903c5cafb23efe16cd226
wverbeke <willem.verbeke@cern.ch>
2018-03-18 22:30:07 +0100

    further implementation of nuisances

--- interface/HistCollectionBaseNuisances.h
+++ interface/HistCollectionBaseNuisances.h
@@ -17,14 +17,23 @@ class HistCollectionBaseNuisances : public HistCollectionBase{
         HistCollectionBaseNuisances(const std::shared_ptr<HistInfo>&, const std::shared_ptr<Sample>&, const std::shared_ptr<Category>&, const std::vector < std::string >&, const bool includeSB = false);
         HistCollectionBaseNuisances(const std::string&, const std::shared_ptr<HistInfo>&, const std::shared_ptr<Sample>&, const std::shared_ptr<Category>&, const std::vector < std::string >&, const bool includeSB = false);
 
-        std::shared_ptr<TH1D> accessNuisance(const size_t nuisanceIndex, const size_t categoryIndex, const bool sb);
-        std::shared_ptr<TH1D> accessNuisance(const size_t nuisanceIndex, const std::vector< size_t >& categoryIndices, const bool sb);
+        std::shared_ptr<TH1D> accessNuisanceUp(const size_t nuisanceIndex, const size_t categoryIndex, const bool sb) const;
+        std::shared_ptr<TH1D> accessNuisanceDown(const size_t nuisanceIndex, const size_t categoryIndex, const bool sb) const;
+        std::shared_ptr<TH1D> accessNuisanceUp(const size_t nuisanceIndex, const std::vector< size_t >& categoryIndices, const bool sb) const;
+        std::shared_ptr<TH1D> accessNuisanceDown(const size_t nuisanceIndex, const std::vector< size_t >& categoryIndices, const bool sb) const;
 
         void negBinsToZero() const;
+        size_t numberOfNuisances() const;
+
+        HistCollectionBaseNuisances& operator+=(const HistCollectionBaseNuisances&);
 
    private:
-        std::vector < std::vector < std::shared_ptr < TH1D > > > nuisanceCollection;
-        std::vector < std::vector < std::shared_ptr < TH1D > > > nuisanceSideBand;
+        std::vector < std::vector < std::shared_ptr < TH1D > > > nuisanceCollectionUp;
+        std::vector < std::vector < std::shared_ptr < TH1D > > > nuisanceCollectionDown;
+        std::vector < std::vector < std::shared_ptr < TH1D > > > nuisanceSideBandUp;
+        std::vector < std::vector < std::shared_ptr < TH1D > > > nuisanceSideBandDown;
+
+        void initNuisanceVectors(const std::vector < std::string >&); 
 };
 
 
