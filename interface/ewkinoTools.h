fab232ce235936017ef133c39a90a9eaf37ca162
willem <willem.verbeke@cern.ch>
2018-04-13 20:30:29 +0200

    add functionality to do check 2016 run era

--- interface/ewkinoTools.h
+++ interface/ewkinoTools.h
@@ -1,7 +1,8 @@
 #ifndef ewkinoTools_H
 #define ewkinoTools_H
 namespace ewk{
-    unsigned runPeriod(const unsigned long);
+    unsigned runPeriod2017(const unsigned long);
+    unsigned runPeriod2016(const unsigned long);
     //Add functions here to determine categories and search regions
 }
 #endif
--- src/ewkinoTools.cc
+++ src/ewkinoTools.cc
@@ -1,7 +1,8 @@
 #include "../interface/ewkinoTools.h"
+#include <iostream>
 
 //return run period for 2017 data
-unsigned ewk::runPeriod(const unsigned long runNb){
+unsigned ewk::runPeriod2017(const unsigned long runNb){
     if(runNb < 297020){
         return 0; //Run A
     } else if(runNb < 299337){
@@ -15,6 +16,30 @@ unsigned ewk::runPeriod(const unsigned long runNb){
     } else if(runNb < 306827){
         return 5; //Run F
     } else{
+        std::cerr << "Error: Unknown run number for 2017 Collisions" << std::endl;
         return 6;
+    }
+}
+
+unsigned ewk::runPeriod2016(const unsigned long runNb){
+    if(runNb < 272006){
+        return 0; //Run A
+    } else if(runNb < 275656){
+        return 1; //Run B
+    } else if(runNb < 276314){
+        return 2; //Run C
+    } else if(runNb <  276830){
+        return 3; //Run D
+    } else if(runNb < 277771){
+        return 4; //Run E
+    } else if(runNb < 278819){
+        return 5; //Run F
+    } else if(runNb < 280918){
+        return 6; //Run G
+    } else if(runNb < 284045){
+        return 7; //Run H
+    } else{
+        std::cerr << "Error: Unknown run number for 2016 Collisions" << std::endl;
+        return 8;
     }
 }
