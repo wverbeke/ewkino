96aef3009b0eed12ce55c4b94744e17b50f673ad
willem <willem.verbeke@cern.ch>
2018-04-23 11:25:09 +0200

    start cleaning up BDT training code

--- bdtTraining/trainBDT.cc
+++ bdtTraining/trainBDT.cc
@@ -9,58 +9,61 @@
 #include "TMVA/DataLoader.h"
 #include "TMVA/Tools.h"
 
-void trainMvaMethods(const std::string& jetsCat = "", const std::string& mllCat = ""){
+void trainMvaMethods(const std::string& jetsCat = "", const std::string& mllCat = "", const std::string& year = ""){
+
+    //check whether the training is for 2016 or 2017
+    if( ! (year == "2016" || year == "2017") ){
+        std::cerr << "Error: specified training year is not 2016 or 2017, returning" << std::endl;
+    } 
+    bool is2016 = (year == "2016");
+
     //output file
-    TFile* outputFile = TFile::Open((const TString&) "trainingOutput" + jetsCat + mllCat +".root", "RECREATE" );
-    TMVA::Factory *factory = new TMVA::Factory( "TMVAClassification", outputFile, "!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=Classification" );
+    TFile* outputFile = TFile::Open( (const TString&) "trainingOutput" + jetsCat + mllCat + "_" + year + ".root", "RECREATE" );
+    TMVA::Factory *factory = new TMVA::Factory("TMVAClassification" + jetsCat + mllCat + "_" + year, outputFile, "!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=Classification" );
 
     //names of categories for which to do the training
-    //const std::string mllNames[nMll] = {"mllInclusive", "onZ", "offZ"};
-    //const std::string catNames[nCat] = {"nJetsInclusive", "0bJets_01Jets", "0bJets_2Jets", "1bJet_01jets", "1bJet_23Jets", "1bJet_4Jets", "2bJets"};
+    TMVA::DataLoader *dataloader = new TMVA::DataLoader("dataset" + jetsCat + mllCat);
 
-
-
-    TMVA::DataLoader *dataloader=new TMVA::DataLoader("dataset");
-    dataloader->AddVariable("asymmetryWlep", 'F'); //mystery crash for "1bJet_01jets" category on this variable
-    dataloader->AddVariable("topMass", 'F');
-    dataloader->AddVariable("etaMostForward", 'F');
-    dataloader->AddVariable("mTW", 'F');
-    dataloader->AddVariable("highestDeepCSV", 'F');
-    if(jetsCat != "1bJet_01jets") dataloader->AddVariable("numberOfJets", 'F');
-    if(jetsCat == "2bJets") dataloader->AddVariable("numberOfBJets", 'F');
-    dataloader->AddVariable("pTLeadingLepton", 'F');
-    if(jetsCat != "1bJet_01jets" && jetsCat != "0bJets_01Jets" && jetsCat != "1bJet_4Jets" && jetsCat != "2bJets") dataloader->AddVariable("pTLeadingBJet", 'F');
+    dataloader->AddVariable("asymmetryWlep", 'F');
+    if(jetsCat != "0bJets01Jets" && jetsCat != "0bJets2Jets") dataloader->AddVariable("deltaRWLeptonTaggedbJet", 'F');
+    dataloader->AddVariable("etaZ", 'F');
+    if(jetsCat != "1bJet01jets" && jetsCat != "0bJets01Jets" && jetsCat != "1bJet4Jets" && jetsCat != "2bJets") dataloader->AddVariable("pTLeadingBJet", 'F');
     dataloader->AddVariable("pTMostForwardJet", 'F');
-    if(jetsCat == "2bJets" || jetsCat == "1bJet_4Jets") dataloader->AddVariable("mAllJets", 'F');
-    if(jetsCat != "0bJets_01Jets" && jetsCat != "1bJet_01jets" && jetsCat != "1bJet_4Jets" && jetsCat != "2bJets") dataloader->AddVariable("maxDeltaPhijj", 'F');
-    if(jetsCat != "0bJets_01Jets" && jetsCat != "1bJet_01jets") dataloader->AddVariable("maxDeltaRjj", 'F');
-    if(jetsCat != "1bJet_01jets" && jetsCat != "0bJets_01Jets") dataloader->AddVariable("maxMjj", 'F');
-    if(jetsCat != "0bJets_01Jets" && jetsCat != "0bJets_2Jets") dataloader->AddVariable("maxMlb", 'F');
-    if(jetsCat != "0bJets_01Jets" && jetsCat != "0bJets_2Jets") dataloader->AddVariable("minMlb", 'F');
-    if(jetsCat != "0bJets_01Jets" && jetsCat != "0bJets_2Jets") dataloader->AddVariable("pTMaxlb", 'F');
-    if(jetsCat != "0bJets_01Jets" && jetsCat != "0bJets_2Jets") dataloader->AddVariable("maxmTbJetMet", 'F');
-    dataloader->AddVariable("maxpTLeptonMet", 'F');
+    dataloader->AddVariable("highestDeepCSV", 'F');
+    dataloader->AddVariable("etaMostForward", 'F');
+    dataloader->AddVariable("pTLeadingLepton", 'F');
     dataloader->AddVariable("m3l", 'F');
-    //dataloader->AddVariable("maxM2l", 'F');           //not used now due to correlation with 3l variables
-    //dataloader->AddVariable("ptMax2l", 'F');          //not used now due to correlation with 3l variables
-    dataloader->AddVariable("pT3l", 'F');
-    dataloader->AddVariable("ht", 'F');
-    if(mllCat == "offZ") dataloader->AddVariable("mZ", 'F');
-    //if(jetsCat != "1bJet_01jets") dataloader->AddVariable("pTForwardJets", 'F');
-    //if(jetsCat != "1bJet_4Jets" && jetsCat != "2bJets") dataloader->AddVariable("pTLeadingJet", 'F');
-    //dataloader->AddVariable("mNotSoForwardJets");
-    //dataloader->AddVariable("missingET", 'F');
-    //dataloader->AddVariable("etaZ", 'F');
-    //dataloader->AddVariable("maxDeltaPhill", 'F');
-    //dataloader->AddVariable("mForwardJetsLeadinBJetW", 'F');
-
+    //if(jetsCat != "0bJets_01Jets" && jetsCat != "0bJets_2Jets") dataloader->AddVariable("maxDeltaPhibmet", 'F');
+    if(jetsCat != "0bJets01Jets" && jetsCat != "1bJet01jets" && jetsCat != "1bJet4Jets" && jetsCat != "2bJets") dataloader->AddVariable("maxDeltaPhijj", 'F');
+    if(jetsCat != "1bJet01jets" && jetsCat != "0bJets01Jets") dataloader->AddVariable("maxMjj", 'F');
+    if(jetsCat != "0bJets01Jets" && jetsCat != "0bJets2Jets") dataloader->AddVariable("minDeltaPhibmet", 'F');
+    if(jetsCat != "0bJets01Jets" && jetsCat != "0bJets2Jets") dataloader->AddVariable("minDeltaPhilb", 'F');
+    if(jetsCat == "1bJet4Jets" || jetsCat == "2bJets") dataloader->AddVariable("minMlb", 'F');
+    if(jetsCat != "0bJets01Jets" && jetsCat != "0bJets2Jets") dataloader->AddVariable("maxMlb", 'F');
+    if(jetsCat == "1bJet4Jets" || jetsCat == "2bJets") dataloader->AddVariable("ht", 'F'); 
+    if(jetsCat == "1bJet4Jets" || jetsCat == "2bJets") dataloader->AddVariable("numberOfJets", 'F');
+    if(jetsCat == "2bJets") dataloader->AddVariable("numberOfBJets", 'F');
+    if(jetsCat != "0bJets01Jets" && jetsCat != "0bJets2Jets") dataloader->AddVariable("maxmTbmet", 'F');
+    //if(jetsCat != "0bJets_01Jets" && jetsCat != "1bJet_01jets") dataloader->AddVariable("maxDeltaRjj", 'F');
+    dataloader->AddVariable("pTmin2l", 'F');
+    //dataloader->AddVariable("pT3l", 'F');
+    dataloader->AddVariable("mTW", 'F');
+    dataloader->AddVariable("topMass", 'F');
+    
+    //set correct weights for every event (depending on the process it comes from)
     dataloader->SetSignalWeightExpression("eventWeight");
     dataloader->SetBackgroundWeightExpression("eventWeight");
 
-    //get training trees
-    TFile* file =  new TFile("../trainingTrees/bdtTrainingTree.root");
-    TTree* signalTree = (TTree*) (file->Get((const TString&) "signalTree" + jetsCat + mllCat));
-    TTree* backgroundTree = (TTree*) (file->Get((const TString&) "backgroundTree" + jetsCat + mllCat));
+    //get training tree
+    std::string directory;
+    if( is2016 ){
+        directory = "trainingTrees_tZq2016";
+    } else {
+        directory = "trainingTrees_tZq2017";
+    } 
+    TFile* inputFile =  TFile::Open( (const TString&) "../" + directory + "/trainingTree.root");
+    TTree* signalTree = (TTree*) (inputFile->Get( (const TString&) "signalTree" + jetsCat + mllCat));
+    TTree* backgroundTree = (TTree*) (inputFile->Get( (const TString&) "backgroundTree" + jetsCat + mllCat));
 
     dataloader->AddSignalTree(signalTree, 1.);
     dataloader->AddBackgroundTree(backgroundTree, 1.);
@@ -69,55 +72,10 @@ void trainMvaMethods(const std::string& jetsCat = "", const std::string& mllCat 
     TCut mycuts = ""; // for example: TCut mycuts = "abs(var1)<0.5 && abs(var2-0.5)<1";
     TCut mycutb = ""; // for example: TCut mycutb = "abs(var1)<0.5";
     dataloader->PrepareTrainingAndTestTree( mycuts, mycutb, "nTrain_Signal=0:nTrain_Background=0:nTest_Signal=0:nTest_Background=0:NormMode=None:SplitMode=Random:!V" );
-    //BDT 
-    //factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTG", "!H:!V:NTrees=1000:MinNodeSize=2.5%:BoostType=Grad:Shrinkage=0.10:UseBaggedBoost:BaggedSampleFraction=0.5:nCuts=20:MaxDepth=2" );
-    factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTGAlt", "!H:!V:NTrees=1000:MinNodeSize=2.5%:BoostType=Grad:Shrinkage=0.10:nCuts=200:MaxDepth=2:IgnoreNegWeightsInTraining=True");
-    factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTGAlt_200trees", "!H:!V:NTrees=200:MinNodeSize=2.5%:BoostType=Grad:Shrinkage=0.10:nCuts=200:MaxDepth=2:IgnoreNegWeightsInTraining=True");
-    factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTGAlt_shrinkage04", "!H:!V:NTrees=1000:MinNodeSize=2.5%:BoostType=Grad:Shrinkage=0.4:nCuts=200:MaxDepth=2:IgnoreNegWeightsInTraining=True");
-    factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTGAlt_minNode005", "!H:!V:NTrees=1000:MinNodeSize=5%:BoostType=Grad:Shrinkage=0.10:nCuts=200:MaxDepth=2:IgnoreNegWeightsInTraining=True");
-    factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTGAlt_negWeights", "!H:!V:NTrees=1000:MinNodeSize=2.5%:BoostType=Grad:Shrinkage=0.10:nCuts=200:MaxDepth=2");
-    factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTGAlt_MaxDepth4", "!H:!V:NTrees=1000:MinNodeSize=2.5%:BoostType=Grad:Shrinkage=0.10:nCuts=200:MaxDepth=4:IgnoreNegWeightsInTraining=True");
-    factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTGAlt_20Cuts", "!H:!V:NTrees=1000:MinNodeSize=2.5%:BoostType=Grad:Shrinkage=0.10:nCuts=20:MaxDepth=4:IgnoreNegWeightsInTraining=True");
-    //factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTGOtherAn", "!H:!V:NTrees=200:BoostType=Grad:Shrinkage=0.4:nCuts=200:MaxDepth=2:IgnoreNegWeightsInTraining=True");
-    //factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDT", "!H:!V:NTrees=1000:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20" ); //850
-    //factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTB", "!H:!V:NTrees=400:BoostType=Bagging:SeparationType=GiniIndex:nCuts=20");
-    //factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTD", "!H:!V:NTrees=400:MinNodeSize=5%:MaxDepth=3:BoostType=AdaBoost:SeparationType=GiniIndex:nCuts=20:VarTransform=Decorrelate" );
 
-    //NN
-    //factory->BookMethod( dataloader, TMVA::Types::kMLP, "MLP", "H:!V:NeuronType=tanh:VarTransform=N:NCycles=600:HiddenLayers=N+5:TestRate=5:!UseRegulator" );
-    // General layout.
-    TString layoutString ("Layout=TANH|128,TANH|128,TANH|128,LINEAR");
-     
-    // Training strategies.
-    TString training0("LearningRate=1e-1,Momentum=0.9,Repetitions=1,"
-    "ConvergenceSteps=20,BatchSize=256,TestRepetitions=10,"
-    "WeightDecay=1e-4,Regularization=L2,"
-    "DropConfig=0.0+0.5+0.5+0.5, Multithreading=True");
-    TString training1("LearningRate=1e-2,Momentum=0.9,Repetitions=1,"
-    "ConvergenceSteps=20,BatchSize=256,TestRepetitions=10,"
-    "WeightDecay=1e-4,Regularization=L2,"
-    "DropConfig=0.0+0.0+0.0+0.0, Multithreading=True");
-    TString training2("LearningRate=1e-3,Momentum=0.0,Repetitions=1,"
-    "ConvergenceSteps=20,BatchSize=256,TestRepetitions=10,"
-    "WeightDecay=1e-4,Regularization=L2,"
-    "DropConfig=0.0+0.0+0.0+0.0, Multithreading=True");
-    TString trainingStrategyString ("TrainingStrategy=");
-    trainingStrategyString += training0 + "|" + training1 + "|" + training2;
-    
-    // General Options.
-    TString dnnOptions ("!H:V:ErrorStrategy=CROSSENTROPY:VarTransform=N:"
-    "WeightInitialization=XAVIERUNIFORM");
-    dnnOptions.Append (":"); dnnOptions.Append (layoutString);
-    dnnOptions.Append (":"); dnnOptions.Append (trainingStrategyString);
-    
-    // Standard implementation, no dependencies.
-    TString stdOptions = dnnOptions + ":Architecture=STANDARD";
-    //factory->BookMethod(dataloader, TMVA::Types::kDNN, "DNN", stdOptions);
-    
-    // Multi-core CPU implementation.
-    //TString cpuOptions = dnnOptions + ":Architecture=CPU";
-    //factory->BookMethod(dataloader, TMVA::Types::kDNN, "DNN CPU", cpuOptions);
-    //
+    //specify BDT to train
+    factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTG_m1Cuts_Depth4_baggedGrad_1000trees_shrinkage0p1", "!H:!V:NTrees=1000:MinNodeSize=2.5%:BoostType=Grad:Shrinkage=0.1:nCuts=-1:MaxDepth=4:IgnoreNegWeightsInTraining:UseBaggedGrad=True:DoBoostMonitor=True");
+
     //train MVAs using the set of training events
     factory->TrainAllMethods();
 
@@ -128,14 +86,16 @@ void trainMvaMethods(const std::string& jetsCat = "", const std::string& mllCat 
     factory->EvaluateAllMethods();
 
     //save the output
+    inputFile->Close();
     outputFile->Close();
 
     delete factory;
     delete dataloader;
 }
-
 int main(int argc, char* argv[]){
-    if(argc > 2) trainMvaMethods(argv[1], argv[2]);
-    else trainMvaMethods("nJetsInclusive", "mllInclusive");
+    if(argc > 2){
+        trainMvaMethods(argv[1], argv[2], argv[3]);
+    }
+
     return 0;
 }
--- bdtTraining/trainingWrapperSubmit.sh
+++ bdtTraining/trainingWrapperSubmit.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-source ../scripts/setCMSSW.sh
+source ./setCMSSW.sh
 
-for jetCat in  0bJets_01Jets 0bJets_2Jets 1bJet_01jets 1bJet_23Jets 1bJet_4Jets 2bJets; do
-    for mllCat in onZ offZ; do
-        > mvaTraining.sh
-        makeSubmit mvaTraining.sh /user/wverbeke/Work/AnalysisCode/ewkino/bdtTraining/
-        echo "./trainBDT $jetCat $mllCat" >> mvaTraining.sh
-        echo "for method in BDT BDTG BDTD BDTB MLP DNN; do" >> mvaTraining.sh
-        echo '  mv dataset/weights/TMVAClassification_${method}.weights.xml submitWeights/${jetCat}_${mllCat}_${method}.weights.xml' >> mvaTraining.sh
-        echo "done">> mvaTraining.sh
-        qsub mvaTraining.sh -q highmem -lnodes=1:ppn=16 -l walltime=20:00:00
+for year in 2016 2017 do;
+    for jetCat in  0bJets_01Jets 0bJets_2Jets 1bJet_01jets 1bJet_23Jets 1bJet_4Jets 2bJets; do
+        for mllCat in onZ offZ; do
+            script=mvaTraining_${jetCat}_${mllCat}_${year}.sh
+            > $script
+            makeSubmit $script $PWD
+            echo "./trainBDT $jetCat $mllCat $year" >> $script
+            echo "for method in BDTG_m1Cuts_Depth4_baggedGrad_1000trees_shrinkage0p1; do" >> $script
+            echo "  mv dataset${jetCat}${mllCat}${year}/weights/TMVAClassification${jetCat}${mllCat}${year}_\${method}.weights.xml submitWeights/${jetCat}_${mllCat}_${year}_\${method}.weights.xml" >> $script
+            echo "done">> $script
+            #qsub $script -q highmem -lnodes=1:ppn=16 -l walltime=40:00:00
+            qsub $script -l walltime=40:00:00
+            rm $script
+        done
     done
 done
-rm mvaTraining.sh
