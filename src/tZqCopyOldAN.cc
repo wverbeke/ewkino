6c2e937e4aa45bac103c6d1e57352f3d56d10e16
willem <willem.verbeke@cern.ch>
2018-05-06 10:46:36 +0200

    fix sample list name

--- src/tZqCopyOldAN.cc
+++ src/tZqCopyOldAN.cc
@@ -39,7 +39,7 @@ void treeReader::setup(){
     setTDRStyle();
     gROOT->SetBatch(kTRUE);
     //read samples and cross sections from txt file
-    readSamples("sampleLists/samples.txt");
+    readSamples("sampleLists/samples2016.txt");
     //info on kinematic distributions to plot
     //name      xlabel    nBins,  min, max
     histInfo = {
