8961f8a34e0a90001a09906a7dcf63c932068e41
willem <willem.verbeke@cern.ch>
2018-07-17 11:30:04 +0200

    add coloring for nonprompt

--- plotting/plotCode.cc
+++ plotting/plotCode.cc
@@ -98,6 +98,7 @@ Color_t bkgColortZq(const std::string& bkgName){
     if(bkgName == "tZq") return kRed - 7;
     if(bkgName == "TT" ||  bkgName == "TT + Jets") return kOrange - 2;
     if(bkgName == "nonprompt" ||  bkgName == "non-prompt") return kOrange - 2;
+    if(bkgName == "Nonprompt" ||  bkgName == "Nonprompt e/#mu") return kOrange - 2;
     if(bkgName == "TT/T + X" || bkgName == "TT + X") return kBlue - 10;
     //if(bkgName == "X + #gamma") return kMagenta - 10;
     if(bkgName == "X + #gamma") return kMagenta - 7;
