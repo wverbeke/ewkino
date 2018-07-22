10ecfc57b5bc3a0f6158b92e9a74f35c03b95d2e
willem <willem.verbeke@cern.ch>
2018-07-22 19:25:47 +0200

    make script to submit and run the whole analysis chain

---
+++ scripts/runAnalysis.sh
@@ -0,0 +1,12 @@
+source setCMSSW.sh
+years=("" 2017)
+for process in tZq controlRegions tZq_lowBDT nonpromptControlRegions; do
+    for year in "${years[@]}"; do
+        script=${process}${year}.sh
+        makeSubmit $script ${PWD}/..
+        echo "make -f makeFiles/make${process}${year}" >> $script
+        echo "./${process}${year}" >> $script
+        #submitJob $script
+        cat $script
+    done
+done
