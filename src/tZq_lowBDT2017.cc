06153a0b81f645768065df051ac594f8d00f6132
willem <willem.verbeke@cern.ch>
2018-07-24 14:43:17 +0200

    deal with 0 pu events properly

--- src/tZq_lowBDT2017.cc
+++ src/tZq_lowBDT2017.cc
@@ -249,6 +249,9 @@ void treeReader::Analyze(){
             //event reweighting
             if( isMC() ){
                 weight *= sfWeight(); 
+                if( weight == 0.){
+                    continue;
+                }
             }
 
             //compute nominal bdt value
