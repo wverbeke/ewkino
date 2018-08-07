73904abf37b0461bafe50fb048688818ede59be8
willem <willem.verbeke@cern.ch>
2018-08-07 19:37:25 +0200

    update ttV lepton selection

--- src/ttVLeptonSelection.cc
+++ src/ttVLeptonSelection.cc
@@ -26,10 +26,10 @@ bool treeReader::lepIsGoodttZ3l2017(const unsigned leptonIndex) const{
     if( !lepIsGoodBase(leptonIndex) ) return false;
     if( closestJetDeepCSV(leptonIndex) >  0.8001) return false;
     if( !passLeptonMva( leptonIndex, 0.4) ){
-        if( _ptRatio[leptonIndex] <= 0.3) return false;
-        if( closestJetDeepCSV( leptonIndex ) >= 0.2) return false;
+        if( _ptRatio[leptonIndex] <= 0.4) return false;
+        if( closestJetDeepCSV( leptonIndex ) >= 0.5) return false;
         if( isElectron(leptonIndex) ){
-            if( _lElectronMva[leptonIndex] <= ( ( fabs(_lEta[leptonIndex]) < 1.479 ) ? 0.0 : 0.3 ) ) return false;
+            if( _lElectronMva[leptonIndex] <= ( ( fabs(_lEta[leptonIndex]) < 1.479 ) ? -0.3 : 0.3 ) ) return false;
         }
     }
     return true;
@@ -67,9 +67,9 @@ bool treeReader::lepIsGoodttW2016(const unsigned leptonIndex) const{
     if( closestJetDeepCSV(leptonIndex) >  0.8958) return false;
     if( !passLeptonMva( leptonIndex, 0.6) ){
         if( _ptRatio[leptonIndex] <= 0.5) return false;
-        if( closestJetDeepCSV( leptonIndex ) >= 0.3) return false;
+        if( closestJetDeepCSV( leptonIndex ) >= 0.5) return false;
         if( isElectron(leptonIndex) ){
-            if( _lElectronMva[leptonIndex] <= ( ( fabs(_lEta[leptonIndex]) < 1.479 ) ? 0.3 : 0.8 ) ) return false;
+            if( _lElectronMva[leptonIndex] <= ( ( fabs(_lEta[leptonIndex]) < 1.479 ) ? -0.6 : -0.2 ) ) return false;
         }
     }
     return true;
@@ -79,10 +79,10 @@ bool treeReader::lepIsGoodttW2017(const unsigned leptonIndex) const{
     if( !lepIsGoodBase(leptonIndex) ) return false;
     if( closestJetDeepCSV(leptonIndex) >  0.8001) return false;
     if( !passLeptonMva( leptonIndex, 0.6) ){
-        if( _ptRatio[leptonIndex] <= 0.4) return false;
-        if( closestJetDeepCSV( leptonIndex ) >= 0.4) return false;
+        if( _ptRatio[leptonIndex] <= 0.5) return false;
+        if( closestJetDeepCSV( leptonIndex ) >= 0.2) return false;
         if( isElectron(leptonIndex) ){
-            if( _lElectronMva[leptonIndex] <= ( ( fabs(_lEta[leptonIndex]) < 1.479 ) ? 0.4 : 0.8 ) ) return false;
+            if( _lElectronMva[leptonIndex] <= ( ( fabs(_lEta[leptonIndex]) < 1.479 ) ? -0.1 : 0.3 ) ) return false;
         }
     }
     return true;
