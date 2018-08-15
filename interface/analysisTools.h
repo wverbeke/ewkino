95fa84a25889558a7bc90a7e912ae85bbd20a659
wverbeke <willem.verbeke@cern.ch>
2018-08-15 07:54:51 +0200

    clean up analysisTools, and remove the functions that were put in other headers

--- interface/analysisTools.h
+++ interface/analysisTools.h
@@ -1,3 +1,7 @@
+/*
+Some simple tools which are useful in most physics analysis tasks
+*/
+
 #ifndef tools_h
 #define tools_h
 
@@ -8,53 +12,16 @@
 #include "TH1D.h"
 #include "TLorentzVector.h"
 
-namespace tools{
+namespace analysisTools{
+
     //show progress bar
     void printProgress(double);
 
     //Set negative histogram bins to zero
-    void setNegativeZero(TH1D*);
-
-    //String manipulation to read sample file
-    void removeBackSpaces(std::string& s);
-    void removeFrontSpaces(std::string&s);
-    void cleanSpaces(std::string& s);
-    std::string extractFirst(std::string& s);
-
-    //translate line from sample file into std::tuple
-    std::tuple<std::string, std::string, double> readSampleLine(std::string& line);
+    void setNegativeBinsToZero(const std::shared_ptr<TH1D>&);
+    void setNegativeBinsToZero(const TH1D*);
 
     //print a combine datacard
     void printDataCard(const double obsYield, const double sigYield, const std::string& sigName, const double* bkgYield, const unsigned nBkg, const std::string* bkgNames, const std::vector<std::vector<double> >& systUnc, const unsigned nSyst, const std::string* systNames, const std::string* systDist, const std::string& cardName, const bool shapeCard = false, const std::string& shapeFileName = "");
-
-    //call std::system, but allow an std::string to be used
-    void system(const std::string&);
-
-    //delete file of given name
-    void deleteFile(const std::string&);
-
-    //get CMSSW directory currently used
-    std::string CMSSWDirectory();
-
-    //initialize a submission script
-    std::ostream& initScript(std::ostream&);
-
-    //sleep for x seconds
-    void sleep(unsigned);
-
-    //submit script of given name and catch submission errors
-    void submitScript(const std::string&, const std::string&); 
-
-    //check whether there are running jobs
-    bool runningJobs(const std::string& jobName = "");
-
-    //get current directory
-    std::string currentDirectory();
-
-    //list all files in a directory and save this to a txt file of given name
-    void makeFileList(const std::string&, const std::string&);
-
-    //add trailing / to directoryName if needed
-    std::string formattedDirectoryName(const std::string&);     
 }
 #endif
