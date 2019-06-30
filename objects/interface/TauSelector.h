d3b65054f9e360bf7b3a3025255cf6cb2b70a6bf
wverbeke <willem.verbeke@cern.ch>
2019-06-30 16:50:08 +0200

    add TauSelector

--- objects/interface/TauSelector.h
+++ objects/interface/TauSelector.h
@@ -1,6 +1,9 @@
 #ifndef TauSelector_H
 #define TauSelector_H
 
+//include c++ library classes
+#include <utility>
+
 //include other parts of code 
 #include "LeptonSelector.h"
 
@@ -9,10 +12,18 @@ class Tau;
 class TauSelector : public LeptonSelector {
 
     public:
-        TauSelector( const Tau& tau ) : tauPtr( &tau ) {}
+        TauSelector( const Tau* tau ) : tauPtr( tau ) {}
+
+        virtual bool isLoose() const override;
+        virtual bool isFO() const override;
+        virtual bool isGood() const override;
+        virtual bool isTight() const override;
 
     private:
         const Tau* const tauPtr;
+
+        virtual LeptonSelector* clone() const & override{ return new TauSelector( *this ); }
+        virtual LeptonSelector* clone() && override{ return new TauSelector( std::move( *this ) ); }
 
 };
 
