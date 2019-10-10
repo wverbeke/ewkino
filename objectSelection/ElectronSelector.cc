5fedb8d651301e4ec8b64c00095de6b7eb80104d
wverbeke <willem.verbeke@cern.ch>
2019-10-10 13:33:25 +0200

    implement ttH lepton selection

--- objectSelection/ElectronSelector.cc
+++ objectSelection/ElectronSelector.cc
@@ -1,16 +1,86 @@
 #include "../objects/interface/ElectronSelector.h"
 
+//include c++ library classes
+#include <cmath>
+
+//include other parts of framework
+#include "bTagWP.h"
+
 
 /*
 loose electron selection
 */
 
+
+double rawElectronMVA( const double electronMVA ){
+	return -0.5 * ( std::log( ( 1. - electronMVA ) / ( 1. + electronMVA ) ) );
+}
+
+
+unsigned electronMVACategory( const Electron& electron ){
+    double absEtaSC = fabs( electron.etaSuperCluster() );
+    if( electron.pt() < 10 ){
+        if( absEtaSC < 0.8 ) return 0;
+        else if( absEtaSC < 1.479 ) return 1;
+        else return 2;
+    } else {
+        if( absEtaSC < 0.8 ) return 3;
+        else if( absEtaSC < 1.479 ) return 4;
+        else return 5;
+    }
+}
+
+
+double looseMVACut( const Electron& electron ){
+	unsigned category = electronMVACategory( electron );
+	if( category == 0 ) return 0.700642584415;
+    else if( category == 1 ) return 0.739335420875;
+    else if( category == 2 ) return 1.45390456109;
+    else if( category == 3 ) return -0.146270871164;
+    else if( category == 4 ) return -0.0315850882679;
+    else return -0.0321841194737; 
+}
+
+
+double WP80MVACut( const Electron& electron ){
+	unsigned category = electronMVACategory( electron );
+	if( category == 0 ) return 3.53495358797 - std::exp( -electron.pt() / 3.07272325141 ) * 9.94262764352;
+	else if( category == 1 ) return 3.06015605623 - std::exp( -electron.pt() / 1.95572234114 ) * 14.3091184421;
+	else if( category == 2 ) return 3.02052519639 - std::exp( -electron.pt() / 1.59784164742 ) * 28.719380105;
+	else if( category == 3 ) return 7.35752275071 - std::exp( -electron.pt() / 15.87907864 ) * 7.61288809226;
+	else if( category == 4 ) return 6.41811074032 - std::exp( -electron.pt() / 14.730562874 ) * 6.96387331587;
+	else return 5.64936312428 - std::exp( -electron.pt() / 16.3664949747 ) * 7.19607610311;
+}
+
+
+bool passMVALoose( const Electron& electron ){
+	double rawMVA = rawElectronMVA( electron.electronMVAFall17NoIso() );
+    return rawMVA > looseMVACut( electron );
+}
+
+
+bool passMVAWP80( const Electron& electron ){
+	double rawMVA = rawElectronMVA( electron.electronMVAFall17NoIso() );
+    return rawMVA > WP80MVACut( electron );
+}
+
+double leptonMVACutElectron(){
+    return 0.8;
+}
+
+
 bool ElectronSelector::isLooseBase() const{
     if( electronPtr->pt() < 10 ) return false;
     if( fabs( electronPtr->eta() ) >= 2.5 ) return false;
+    if( fabs( electronPtr->dxy() ) >= 0.05 ) return false;
+    if( fabs( electronPtr->dz() ) >= 0.1 ) return false;
+    if( electronPtr->sip3d() >= 8 ) return false;
+    if( electronPtr->numberOfMissingHits() >= 2 ) return false;
+    if( electronPtr->miniIso() >= 0.4 ) return false;
     if( ! electronPtr->passDoubleEGEmulation() ) return false;
     return true;
 }
+
 
 
 bool ElectronSelector::isLoose2016() const{ 
@@ -34,21 +104,31 @@ FO electron selection
 
 bool ElectronSelector::isFOBase() const{
     if( !isLoose() ) return false;
+    if( electronPtr->numberOfMissingHits() > 0 ) return false;
+    if( electronPtr->leptonMVAttH() <= leptonMVACutElectron() ){
+        if( !passMVAWP80( *electronPtr ) ) return false;
+    } else {
+        if( !passMVALoose( *electronPtr ) ) return false;
+    }
+    if( !electronPtr->passConversionVeto() ) return false;
     return true;
 }
 
 
 bool ElectronSelector::isFO2016() const{
+    if( electronPtr->closestJetDeepFlavor() >= bTagWP::mediumDeepFlavor2016() ) return false;
     return true;
 }
 
 
 bool ElectronSelector::isFO2017() const{
+    if( electronPtr->closestJetDeepFlavor() >= bTagWP::mediumDeepFlavor2017() ) return false;
     return true;
 }
 
 
 bool ElectronSelector::isFO2018() const{
+    if( electronPtr->closestJetDeepFlavor() >= bTagWP::mediumDeepFlavor2018() ) return false;
     return true;
 }
 
@@ -59,7 +139,7 @@ tight electron selection
 
 bool ElectronSelector::isTightBase() const{
     if( !isFO() ) return false;
-    if( !electronPtr->isTightPOGElectron() ) return false;
+    if( electronPtr->leptonMVAttH() <= leptonMVACutElectron() ) return false;
     return true;
 }
 
@@ -84,5 +164,5 @@ cone correction
 */
 
 double ElectronSelector::coneCorrection() const{
-    return 1;
+    return ( 0.9 / electronPtr->ptRatio() );
 }
--- objectSelection/MuonSelector.cc
+++ objectSelection/MuonSelector.cc
@@ -1,13 +1,20 @@
 #include "../objects/interface/MuonSelector.h"
 
+//b-tagging working points
+#include "bTagWP.h"
+
 
 /*
 loose muon selection
 */
 
 bool MuonSelector::isLooseBase() const{
-    if( muonPtr->pt() < 5 ) return false;
+    if( muonPtr->pt() <= 5 ) return false;
     if( fabs( muonPtr->eta() ) >= 2.4 ) return false; 
+    if( fabs( muonPtr->dxy() ) >= 0.05 ) return false;
+    if( fabs( muonPtr->dz() ) >= 0.1 ) return false;
+    if( muonPtr->sip3d() >= 8 ) return false;
+    if( muonPtr->miniIso() >= 0.4 ) return false;
     if( !muonPtr->isLoosePOGMuon() ) return false;
     return true;
 }
@@ -32,24 +39,45 @@ bool MuonSelector::isLoose2018() const{
 FO muon selection
 */
 
+//interpolation between loose and medium working point of deep flavor from 20 to 45 GeV in muon pT as defined in the ttH analysis
+double slidingDeepFlavorThreshold( const double looseWP, const double mediumWP, const double pt ){
+    static const double minPt = 20.;
+    static const double maxPt = 45.;
+    if( pt < minPt ){
+        return mediumWP;
+    } else if( pt > maxPt ){
+        return looseWP;
+    } else {
+        return ( mediumWP - ( mediumWP - looseWP ) / ( maxPt - minPt ) * ( pt - minPt ) );
+    }
+}
+
+
 bool MuonSelector::isFOBase() const{
     if( !isLoose() ) return false;
-    if( !muonPtr->isMediumPOGMuon() ) return false;
+    //currently no medium muon applied in FO, as in ttH AN!
+    if( muonPtr->pt() <= 10 ) return false;
     return true;
 }
 
 
 bool MuonSelector::isFO2016() const{
+    double deepFlavorCut = slidingDeepFlavorThreshold( bTagWP::looseDeepFlavor2016(), bTagWP::mediumDeepFlavor2016(), muonPtr->pt() );
+    if( muonPtr->closestJetDeepFlavor() >= deepFlavorCut ) return false;
     return true;
 }
 
 
 bool MuonSelector::isFO2017() const{
+    double deepFlavorCut = slidingDeepFlavorThreshold( bTagWP::looseDeepFlavor2017(), bTagWP::mediumDeepFlavor2017(), muonPtr->pt() );
+    if( muonPtr->closestJetDeepFlavor() >= deepFlavorCut ) return false;
     return true;
 }
 
 
 bool MuonSelector::isFO2018() const{
+    double deepFlavorCut = slidingDeepFlavorThreshold( bTagWP::looseDeepFlavor2018(), bTagWP::mediumDeepFlavor2018(), muonPtr->pt() );
+    if( muonPtr->closestJetDeepFlavor() >= deepFlavorCut ) return false;
     return true;
 }
 
@@ -60,7 +88,8 @@ tight muon selection
 
 bool MuonSelector::isTightBase() const{
     if( !isFO() ) return false;
-    if( !muonPtr->isTightPOGMuon() ) return false;
+    if( !muonPtr->isMediumPOGMuon() ) return false;
+    if( muonPtr->leptonMVAttH() <= 0.85 ) return false;
     return true;
 }
 
@@ -86,5 +115,5 @@ cone correction
 
 
 double MuonSelector::coneCorrection() const{
-    return 1;
+    return ( 0.8 / muonPtr->ptRatio() );
 }
