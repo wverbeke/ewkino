c94a37fb9ab36d0c49894a4be59f660afd6e05a5
wverbeke <willem.verbeke@cern.ch>
2020-01-28 14:19:30 +0100

    missing file

---
+++ weights/interface/ReweighterBTagLightFlavor.h
@@ -0,0 +1,16 @@
+#ifndef ReweighterBTagLightFlavor_H
+#define ReweighterBTagLightFlavor_H
+
+
+class ReweighterBTagLightFlavor : public ReweighterBTag {
+
+    public:
+        ReweighterBTagLightFlavor(const std::string& weightDirectory, const std::string& sfFilePath, const std::string& workingPoint, const std::shared_ptr< TH2 >& efficiencyUDSG );
+
+
+    private:
+        std::shared_ptr< TH2 > bTagEfficiencyUDSG;
+        virtual double efficiencyMC( const Jet& ) const override;
+};
+
+#endif
