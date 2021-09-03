0cbc0b587592dbef098f84cd1fb7e7eacd5b15a7
LukaLambrecht <Luka.Lambrecht@UGent.be>
2021-09-03 08:13:07 +0200

    minor style update, need to commit before switching to master

--- AnalysisCode/skimming/trileptonskim_multiple.py
+++ AnalysisCode/skimming/trileptonskim_multiple.py
@@ -5,9 +5,12 @@ import os
 
 cmdlist = []
 
-cmdlist.append('python trileptonskim.py /pnfs/iihe/cms/store/user/wverbeke/heavyNeutrino/ ../samplelists/samplelist_tzq_2016_MC.txt ~/Files/trileptonskim/2016MC')
-cmdlist.append('python trileptonskim.py /pnfs/iihe/cms/store/user/wverbeke/heavyNeutrino/ ../samplelists/samplelist_tzq_2017_MC.txt ~/Files/trileptonskim/2017MC')
-cmdlist.append('python trileptonskim.py /pnfs/iihe/cms/store/user/wverbeke/heavyNeutrino/ ../samplelists/samplelist_tzq_2018_MC.txt ~/Files/trileptonskim/2018MC')
+cmdlist.append('python trileptonskim.py /pnfs/iihe/cms/store/user/wverbeke/heavyNeutrino/ '
+		+'../samplelists/samplelist_tzq_2016_MC.txt ~/Files/trileptonskim/2016MC')
+cmdlist.append('python trileptonskim.py /pnfs/iihe/cms/store/user/wverbeke/heavyNeutrino/ '
+		+'../samplelists/samplelist_tzq_2017_MC.txt ~/Files/trileptonskim/2017MC')
+cmdlist.append('python trileptonskim.py /pnfs/iihe/cms/store/user/wverbeke/heavyNeutrino/ '
+		+'../samplelists/samplelist_tzq_2018_MC.txt ~/Files/trileptonskim/2018MC')
 
 for cmd in cmdlist:
     os.system(cmd)
