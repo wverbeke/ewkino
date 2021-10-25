6c8b85fb1162390315bf4eaf689993b25dd60e10
LukaLambrecht <Luka.Lambrecht@UGent.be>
2021-10-25 17:31:03 +0200

    fix incomplete addition of leptonMVATOP to master branch (see another recent commit)

--- objects/src/LightLepton.cc
+++ objects/src/LightLepton.cc
@@ -52,7 +52,8 @@ LightLepton::LightLepton( const LightLepton& rhs, LeptonSelector* leptonSelector
     _closestJetDeepFlavor( rhs._closestJetDeepFlavor ),
     _closestJetTrackMultiplicity( rhs._closestJetTrackMultiplicity ),
     _leptonMVAtZq( rhs._leptonMVAtZq ),
-    _leptonMVAttH( rhs._leptonMVAttH )
+    _leptonMVAttH( rhs._leptonMVAttH ),
+    _leptonMVATOP( rhs._leptonMVATOP)
     {}
 
 
@@ -68,12 +69,13 @@ LightLepton::LightLepton( LightLepton&& rhs, LeptonSelector* leptonSelector ):
     _closestJetDeepFlavor( rhs._closestJetDeepFlavor ),
     _closestJetTrackMultiplicity( rhs._closestJetTrackMultiplicity ),
     _leptonMVAtZq( rhs._leptonMVAtZq ),
-    _leptonMVAttH( rhs._leptonMVAttH )
+    _leptonMVAttH( rhs._leptonMVAttH ),
+    _leptonMVATOP( rhs._leptonMVATOP)
     {}
 
 
 std::ostream& LightLepton::print( std::ostream& os ) const{
     Lepton::print( os );
-    os << " relIso 0.3 = " << _relIso0p3 << " / relIso 0.4 = " << _relIso0p4 << " / miniIso = " << _miniIso << " / miniIsoCharged = " << _miniIsoCharged << " / ptRatio = " << _ptRatio << " / ptRel = " << _ptRel << " / closestJetDeepCSV = " << _closestJetDeepCSV << " / closestJetDeepFlavor = " << _closestJetDeepFlavor << " / closestJetTrackMultiplicity = " << _closestJetTrackMultiplicity << " / leptonMVAtZq = " << _leptonMVAtZq << " / leptonMVAttH = " << _leptonMVAttH;
+    os << " relIso 0.3 = " << _relIso0p3 << " / relIso 0.4 = " << _relIso0p4 << " / miniIso = " << _miniIso << " / miniIsoCharged = " << _miniIsoCharged << " / ptRatio = " << _ptRatio << " / ptRel = " << _ptRel << " / closestJetDeepCSV = " << _closestJetDeepCSV << " / closestJetDeepFlavor = " << _closestJetDeepFlavor << " / closestJetTrackMultiplicity = " << _closestJetTrackMultiplicity << " / leptonMVAtZq = " << _leptonMVAtZq << " / leptonMVAttH = " << _leptonMVAttH << " / leptonMVATOP = " << _leptonMVATOP;
     return os;
 }
