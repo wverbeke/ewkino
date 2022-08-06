f58bf27f370d25b0f2848ad81c14f79b0a4ad41a
LukaLambrecht <Luka.Lambrecht@UGent.be>
2022-08-06 10:27:53 +0200

    adapt cone correction factor measurement to UL

---
+++ fakeRate/fillConeCorrectionFactor.cc
@@ -0,0 +1,160 @@
+/*
+Code to determine the cone correction factor (also known as 'magic factor') 
+in the lepton cone-correction when using a lepton MVA discriminator.
+FO leptons are cone-corrected by dividing their pT by pTRatio( = pT^lepton/pT^jet ) 
+to effectively become pT^jet.
+The border between FO and tight is a lepton MVA cut, and we want to avoid a discontinuity 
+in the pT spectrum as a function of the lepton MVA. 
+This is what the cone correction factor attempts to address.
+*/
+
+
+//include c++ library classes 
+#include <stdexcept>
+
+//include ROOT classes
+#include "TH1D.h"
+#include "TF1.h"
+
+//include other parts of framework
+#include "interface/fakeRateTools.h"
+#include "../TreeReader/interface/TreeReader.h"
+#include "../Event/interface/Event.h"
+#include "../Tools/interface/analysisTools.h"
+#include "../Tools/interface/systemTools.h"
+#include "../Tools/interface/stringTools.h"
+
+
+void determineConeCorrectionFactor( 
+	const std::string& leptonFlavor, 
+	const std::string& year, 
+	const std::string& leptonMVA, 
+	const double mvaThreshold, 
+	const std::string& sampleDirectory, 
+	const std::string& sampleList,
+	const unsigned sampleIndex ){
+    // determine the 'cone correction factor' for a given lepton flavor, lepton MVA and threshold.
+    // leptonMVA is an string to identifiy which MVA to use,
+    // see below for allowed values!
+
+    // check flavour and year
+    fakeRate::checkFlavorString( leptonFlavor );
+    bool isMuon = ( leptonFlavor == "muon" );
+    analysisTools::checkYearString( year );
+    // check mva
+    if( !(  leptonMVA=="leptonMVAttH" 
+	    || leptonMVA=="leptonMVAtZq" 
+	    || leptonMVA=="leptonMVATOP"
+	    || leptonMVA=="leptonMVATOPUL"
+	    || leptonMVA=="leptonMVATOPv2UL" ) ){
+	std::cerr << "ERROR: choice of leptonMVA '"<<leptonMVA<<"' not recognized." << std::endl;
+	return;
+    }
+
+    // initialize histograms
+    const unsigned numberOfBins = 80;
+    std::shared_ptr< TH1D > pTWeightedLeptonMVAHistogram = std::make_shared< TH1D >( 
+	"pTLeptonMVA", "pTLeptonMVA;lepton MVA;Average p_{T}^{cone} (GeV)", numberOfBins, -1, 1 );
+    pTWeightedLeptonMVAHistogram->Sumw2();
+    std::shared_ptr< TH1D > leptonMVAHistogram = std::make_shared< TH1D >( 
+	"leptonMVA", "leptonMVA;lepton MVA;Events", numberOfBins, -1, 1 );
+    leptonMVAHistogram->Sumw2();
+
+    // make tree reader and set to correct sample
+    std::cout << "creating TreeReader and setting to sample no. " << sampleIndex << std::endl;
+    TreeReader treeReader( sampleList, sampleDirectory );
+    treeReader.initSample();
+    for( unsigned i=1; i<=sampleIndex; ++i){
+        treeReader.initSample();
+    }
+
+    // extra check on year
+    if( (year=="2016" && !treeReader.is2016()) ||
+	(year=="2016PreVFP" && !treeReader.is2016PreVFP()) ||
+	(year=="2016PostVFP" && !treeReader.is2016PostVFP()) ||
+	(year=="2017" && !treeReader.is2017()) ||
+	(year=="2018" && !treeReader.is2018()) ){
+	std::cerr << "ERROR: given year and sample year do not agree..." << std::endl;
+	return;
+    }
+
+    // loop over events in sample
+    //long unsigned nentries = treeReader.numberOfEntries();
+    long unsigned nentries = 10000; // for testing
+    std::cout << "starting event loop for " << nentries << " events." << std::endl;
+    for( long unsigned entry = 0; entry < nentries; ++entry ){
+        Event event = treeReader.buildEvent( entry );
+
+        // select loose leptons
+        event.selectLooseLeptons();
+        event.cleanElectronsFromLooseMuons();
+        
+        for( const auto& leptonPtr : event.lightLeptonCollection() ){
+
+            // select correct lepton flavor
+            if( isMuon && !leptonPtr->isMuon() ) continue;
+            if( !isMuon && !leptonPtr->isElectron() ) continue;
+
+            // apply baseline pT cut 
+            if( leptonPtr->uncorrectedPt() <= 10 ) continue;
+
+            // apply cone-correction to leptons failing the MVA cut 
+            double ptVal = leptonPtr->pt();
+	    double mvaVal = (leptonMVA=="leptonMVAttH") ? leptonPtr->leptonMVAttH() : 
+			    (leptonMVA=="leptonMVAtZq") ? leptonPtr->leptonMVAtZq() :
+			    (leptonMVA=="leptonMVATOP") ? leptonPtr->leptonMVATOP() :
+			    (leptonMVA=="leptonMVATOPUL") ? leptonPtr->leptonMVATOPUL() :
+			    (leptonMVA=="leptonMVATOPv2UL") ? leptonPtr->leptonMVATOPv2UL() : 0.0;
+            if( mvaVal <= mvaThreshold ){
+                ptVal /= leptonPtr->ptRatio();
+            }
+
+	    // fill histograms
+	    double weight = (event.weight()>0.) ? 1 : -1;
+            pTWeightedLeptonMVAHistogram->Fill( mvaVal , weight*ptVal );
+            leptonMVAHistogram->Fill( mvaVal , weight );
+	}
+    }
+    std::cout<<"finished event loop"<<std::endl;
+
+    std::string fileName = "coneCorrectionFactor_" + leptonMVA + "_" + leptonFlavor + "_" + year;
+    fileName.append("_histograms_sample_"+std::to_string(sampleIndex)+".root");
+    TFile* histogramFile = TFile::Open( fileName.c_str(), "RECREATE" );
+
+    pTWeightedLeptonMVAHistogram->Write();
+    leptonMVAHistogram->Write();
+
+    histogramFile->Close();
+}
+
+int main( int argc, char* argv[] ){
+
+    std::cerr << "###starting###" << std::endl;
+
+    std::vector< std::string > argvStr( &argv[0], &argv[0] + argc );
+
+    if( argc != 8 ){
+        std::cerr << argc - 1 << " command line arguments given, while 7 are expected." << std::endl;
+        std::cerr << "  - lepton flavor" << std::endl;
+	std::cerr << "  - data taking year" << std::endl;
+	std::cerr << "  - lepton MVA identifier" << std::endl;
+	std::cerr << "  - numerical MVA threshold" << std::endl;
+	std::cerr << "  - sample directory" << std::endl;
+	std::cerr << "  - sample list" << std::endl; 
+	std::cerr << "  - sample index" << std::endl;
+	return 1;
+    }
+    
+    std::string leptonFlavor = argvStr[1];
+    std::string year = argvStr[2];
+    std::string leptonMVA = argvStr[3];
+    double mvaThreshold = std::stod( argvStr[4] );
+    std::string sampleDirectory = argvStr[5];
+    std::string sampleList = argvStr[6];
+    int sampleIndex = std::stoi( argvStr[7] );
+    determineConeCorrectionFactor( leptonFlavor, year, leptonMVA, mvaThreshold,
+                                   sampleDirectory, sampleList, sampleIndex );
+
+    std::cerr << "###done###" << std::endl;
+    return 0;
+}
---
+++ fakeRate/fillConeCorrectionFactor.py
@@ -0,0 +1,94 @@
+######################################################
+# a python submitter for fillConeCorrectionFactor.cc #
+######################################################
+import sys
+import os
+# import job submission tools for qsub
+sys.path.append(os.path.abspath('../skimmer'))
+from jobSubmission import submitQsubJob, initializeJobScript
+# import job submission tools for condor
+sys.path.append(os.path.abspath('../jobSubmission'))
+import condorTools as ct
+
+# set global properties
+runmode = 'condor'
+# (choose from 'local', 'qsub' or 'condor')
+flavours = ['electron','muon']
+# (choose lepton flavours to include)
+leptonMVAs = ['leptonMVATOPUL']
+# (pick a lepton MVA identifier, see below for allowed values)
+mvathresholds = [0.9,0.9] 
+# (tight thresholds; order and length must correspond to flavours)
+years = ['2016PreVFP','2016PostVFP','2017','2018']
+# (choose the data taking years to include)
+samplelistdirectory = os.path.abspath('sampleListsUL/')
+# (set the sample list directory, see also below for the expected sample list name!)
+sampledirectory = '/pnfs/iihe/cms/store/user/llambrec/ntuples_fakerate_UL/'
+# (set the directory where the samples are stored)
+
+# check value of leptonMVA
+for leptonMVA in leptonMVAs:
+    if not (leptonMVA=='leptonMVAttH' 
+	or leptonMVA=='leptonMVAtZq' 
+	or leptonMVA=='leptonMVATOP'
+	or leptonMVA=='leptonMVATOPUL'
+	or leptonMVA=='leptonMVATOPv2UL'):
+	raise Exception('ERROR: choice of leptonMVA "{}" not recognized.'.format(leptonMVA))
+
+# check if executable exists
+exe = './fillConeCorrectionFactor'
+if not os.path.exists(exe):
+    raise Exception('ERROR: executable "{}" does not exist.'.format(exe))
+
+# check input directory
+if not os.path.exists(sampledirectory):
+    raise Exception('ERROR: sample directory {} does not exist.'.format(sampledirectory))
+
+# check sample lists
+samplelists = {}
+for year in years:
+    samplelist = 'samples_conecorrectionfactor_{}.txt'.format(year) 
+    samplelist = os.path.join(samplelistdirectory,samplelist)
+    if not os.path.exists(samplelist):
+	raise Exception('ERROR: sample list {} does not exist.'.format(samplelist))
+    samplelists[year] = samplelist
+
+# loop and submit jobs
+cwd = os.getcwd()
+commands = []
+# loop over flavours and corresponding MVA thresholds
+for flavour,mvathreshold in zip(flavours,mvathresholds):
+    # loop over years
+    for year in years:
+	# check number of samples
+	samplelist = samplelists[year]
+	samplecounter = 0
+	with open(samplelist) as sf:
+	    for sl in sf:
+		if(sl[0] == '#'): continue
+		if(sl[0] == '\n'): continue
+		samplecounter += 1
+	print('found {} samples for {} {}s'.format(samplecounter, year, flavour))
+	# loop over MVAs
+	for leptonMVA in leptonMVAs:
+	    # loop over samples
+	    for i in range(samplecounter):
+		# make the command
+		command = exe +' {} {} {} {} {} {} {}'.format(flavour, year,
+                                                          leptonMVA, mvathreshold,
+                                                          sampledirectory, samplelist, i)
+		commands.append(command)
+		# run local or submit using qsub
+		if( runmode=='local' or runmode=='qsub' ):
+		    script_name = 'qjob_fillConeCorrectionFactor.sh'
+		    with open(script_name,'w') as script:
+			initializeJobScript(script, cmssw_version='CMSSW_10_6_29')
+			script.write('cd {}\n'.format(cwd))
+			script.write(command+'\n')
+		    if runmode=='qsub': submitQsubJob(script_name)
+		    elif runmode=='local': os.system('bash '+script_name)
+
+# submit using condor
+if runmode=='condor':
+    ct.submitCommandsAsCondorCluster('cjob_fillConeCorrectionFactor', commands,
+				     docmsenv=True, cmssw_version='~/CMSSW_10_6_29')
