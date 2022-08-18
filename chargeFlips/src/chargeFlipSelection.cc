1d17365e877172fc8ad91f9b9b2159fc84ebc0f3
LukaLambrecht <Luka.Lambrecht@UGent.be>
2022-08-18 12:03:56 +0200

    intermezzo: update chargeFlip code to new paradigm for later use; not yet used or thoroughly tested; also python plotting code still to be added

--- chargeFlips/src/chargeFlipSelection.cc
+++ chargeFlips/src/chargeFlipSelection.cc
@@ -1,6 +1,7 @@
+// include interface
 #include "../interface/chargeFlipSelection.h"
 
-//include other parts of framework
+// include other parts of framework
 #include "../../objects/interface/Electron.h"
 #include "../../Event/interface/Event.h"
 #include "../../Event/interface/ElectronCollection.h"
@@ -8,54 +9,54 @@
 
 
 bool chargeFlips::passChargeRequirements( const Electron& electron ){
-    if( !electron.isTight() ){
-        return false;
-    }
-    return electron.passChargeConsistency();
+    if( !electron.isTight() ) return false;
+    if( !electron.passChargeConsistency() ) return false;
+    return true;
 }
 
 
 bool chargeFlips::passChargeRequirements( const Muon& muon ){
-    if( !muon.isTight() ){
-        return false;
-    }
-    return ( fabs( muon.trackPtError() / muon.trackPt() ) < 0.2 );
+    if( !muon.isTight() ) return false;
+    if( fabs( muon.trackPtError() / muon.trackPt() ) > 0.2 ) return false;
+    return true;
 }
 
 
-bool chargeFlips::passChargeFlipEventSelection( Event& event, const bool diElectron, const bool onZ, const bool bVeto){
+bool chargeFlips::passChargeFlipEventSelection( Event& event, 
+    const bool diElectron, const bool onZ, const bool bVeto){
 
-    //don't consider taus here
+    // remove taus
     event.removeTaus();
 
-    //baseline lepton selection
+    // baseline lepton selection
     event.cleanElectronsFromLooseMuons();
     event.selectLooseLeptons();
 
-    //clean jets from leptons before tightening selection
+    // clean jets from leptons before tightening selection
     event.cleanJetsFromFOLeptons();
     event.selectGoodJets();
     
-    //select 2 leptons passing tight + charge requirements and veto third lepton
+    // select two leptons passing tight
     if( event.numberOfLightLeptons() != 2 ) return false;
     event.selectTightLeptons();
 
-    //apply charge requirements
+    // apply charge requirements
     //event.selectElectrons( chargeFlips::passChargeRequirements );
     //event.selectMuons( chargeFlips::passChargeRequirements );
 
+    // veto a third lepton
     if( event.numberOfLightLeptons() != 2 ) return false;
 
-    //if specified require that both leptons are electrons
+    // if specified require that both leptons are electrons
     if( diElectron && event.numberOfElectrons() != 2 ) return false;
 
-    //if specified require the invariant mass to be compatible with the Z
+    // if specified require the invariant mass to be compatible with the Z
     if( onZ ){
         double mll = ( event.electron( 0 ) + event.electron( 1 ) ).mass();
         if( fabs( mll - particle::mZ ) >= 10. ) return false;
     }
 
-    //if specified select jets and veto presence of b-jet
+    // if specified select jets and veto presence of b-jet
     if( bVeto ){
         if( event.numberOfMediumBTaggedJets() != 0 ) return false;
     }
